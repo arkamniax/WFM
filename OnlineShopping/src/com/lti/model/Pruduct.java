1f6a12a0e28685bb87485afa76cf8afc863f1f97
arkamniax <arkamniax@users.noreply.github.com>
2020-10-16 09:33:35 +0530

    product class created

---
+++ OnlineShopping/src/com/lti/model/Pruduct.java
@@ -0,0 +1,33 @@
+package com.lti.model;
+
+public class Pruduct {
+	int productId;
+	String productName;
+	String productDesc;
+	int productPrice;
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductDesc() {
+		return productDesc;
+	}
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	public int getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+
+}
